a36e08d5b384952011b84916889f5b387ec4dc03
Alejandro Dobles <alejandro.d.96@gmail.com>
2019-03-04 11:19:31 -0800

    init commit

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 
-T = 5
+T = 5*12
 mu = 0.5
 n = 1000
 
@@ -10,30 +10,31 @@ theta = 1. / T * math.ceil(mu * n)
 def sample_S(T, mu, n):
     # CPG
     pass
-    
+
 def sample_I():
     # CPG
     # call q_i_n and q_n
-    # returns T-vector with values between 1 and n. Element is the id of the firm that defaults at that timestep. 0 if no one defaults
+    # returns T-vector with values between 1 and n. Element is the id of the
+    # firm that defaults at that timestep. 0 if no one defaults
     pass
- 
+
 ###
- 
+
 # FDE
 
 def p_i_n(t, Mt):
     pass
-    
+
 def p_n(t, state_B):
     pass
-    
+
 # JCS
-    
+
 def q_i_n(t, state_b):
     pass
 
 def q_n(t, theta, Sn):
-    pass  
+    pass
 
 ###
 
@@ -46,12 +47,21 @@ def Z_T(theta, Sn, I):
     #call DT
     pass
 
+def I_to_M(I):
+    M = np.zeros((n, T))
+    for i in range(T):
+        if I[i] > 0:
+            M[I[i], i:] = 1
+    return M
+
 def D_T(I):
     # ADS
     # mask = int(I != 0)
-    pass   
-  
-# JCS  
+    # eqns 26 and 27
+    p_n(t, )
+    pass
+
+# JCS
 def run_IS_algorithm():
     # generate event times using poisson
     # for each Sm draw Im
