aea3361abcbc430f20bfb5f66334d8e1ed7ebe08
Colin Gaffney <cgaffney@stanford.edu>
2019-03-05 11:17:53 -0800

    latest changes

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 
-T = 5 * 365
+T = 5 * 12
 mu = 0.01
 n = 1000
 
@@ -14,17 +14,18 @@ sigma_tilde = np.random.uniform(0, 0.2, n)
 X_0 = c # Make sure to review if correct.
 
 # Construct sigma array:
-sigma = [min(np.sqrt(2 * kappa[i] * c[i]), sigma_tilde[i]) for x in range(n)]
+sigma = [min(np.sqrt(2 * kappa[i] * c[i]), sigma_tilde[i]) for i in range(n)]
 
 # Construct gamma array:
 gamma = [np.sqrt(kappa[i]**2 + (2 * sigma[i]**2)) for i in range(n)]
 
 # Beta:
-beta = np.random.uniform(0, 0.01, (n, n)) / 10.0
+beta = np.random.uniform(0, 0.01, (n, n)) / 10.
 
 def sample_S():
     # CPG
     # event times up to T with rate theta
+    print('Sampling S')
     event_times = []
     t = 0
     while len(event_times) < n:
@@ -38,22 +39,26 @@ def sample_I(event_times):
     # call q_i_n and q_n
     # returns T-vector with values between 1 and n. Element is the id of the firm that defaults at that timestep. 0 if no one defaults
 
-    M = np.zeros(n, T)
-
+    M = np.zeros((n, T))
+    print('Sampling I')
     for i, Sm in enumerate(event_times):
         if Sm > T:
             break
+        print((i, Sm))
         if i == 0:
             prev_state = np.zeros_like(M[:, 0])
         else:
             prev_state = M[:, event_times[i-1]]
-        q = [q_i_n(Sm, prev_state) / q_n(Sm, prev_state) for _ in range(n)]
+        q = q_i_n(Sm, prev_state) / q_n(Sm, prev_state)
         transition_idx = np.argmax(np.random.multinomial(1, q))
-        M[transition_idx, Sm:] = np.ones_like(M[transition_idx, Sm:])      
-    transitions = np.argmax(M != 0, axis=0)
-    return transitions
-
-
+        M[transition_idx, Sm:] = np.ones_like(M[transition_idx, Sm:])
+    
+    I = np.zeros_like(M[0])
+    for i in range(M.shape[0]):
+        default_time = np.argmax(M[i])
+        if default_time != 0:
+            I[default_time] = int(i + 1)
+    return I
 
 ###
 
@@ -61,20 +66,19 @@ def sample_I(event_times):
 
 # Create an array, of size n (number of firms):
 def p_i_n(t, Mt):
-    p_i_n = [0 for x in range(n)]
+    #t /= 12.
+    p_i_n = np.zeros(n)
     for i in range(n):
         first_num = 4 * X_0[i] * gamma[i]**2 * np.exp(gamma[i] * t)
-        first_denom = gamma[i] - kappa[i] + (gamma[i] + kappa[i]) * np.exp(gamma[i] * t)**2
+        first_denom = (gamma[i] - kappa[i] + (gamma[i] + kappa[i]) * np.exp(gamma[i] * t)) ** 2
         second_num = 2 * kappa[i] * c[i] * (np.exp(gamma[i] * t) - 1)
         second_denom = gamma[i] - kappa[i] + (gamma[i] + kappa[i]) * np.exp(gamma[i] * t)
-        third = [0 for x in range(n * n)]
+        third = 0.
         for j in range(n):
             if i != j:
-                third[i * j] = beta[i, j] * Mt[j]
-            else:
-                third[i * j] = 0
-        p_i_n[i] = (first_num / first_denom) + (second_num / second_denom) + np.sum(third)
-    return(p_i_n)
+                third += beta[i, j] * Mt[j]
+        p_i_n[i] = (first_num / first_denom) + (second_num / second_denom) + third
+    return p_i_n
 
 
 # In[23]:
@@ -83,16 +87,19 @@ def p_i_n(t, Mt):
 # Sum of individual p_i_n (at a given time-step):
 def p_n(t, state_B):
     probs = p_i_n(t, state_B)
-    return(np.sum(probs))
+    #print(probs)
+    print(np.sum(probs))
+    return np.sum(probs)
 
 # JCS
 
 def q_i_n(t, state_b):
     num = p_i_n(t,state_b)
     denom = p_n(t,state_b)
-    if num == 0 and denom == 0:
-        return 0
-    return (num/denom)*theta
+    if denom == 0:
+        return num
+    else:
+        return num / denom * theta
 
 
 def q_n(t, state_b):
@@ -102,15 +109,14 @@ def q_n(t, state_b):
 # JCS
 
 def I_to_CT(I):
-    CT = [np.count_nonzero(I[:i]) for i in range(len(I))]
-    CT = CT[1:]
-    return np.array(CT)
+    CT = np.count_nonzero(I)
+    return CT
 
 def I_to_M(I):
     M = np.zeros((n, T))
     for i in range(T):
         if I[i] > 0:
-            M[I[i], i:] = 1
+            M[int(I[i]) - 1, i:] = 1
     return M
 
 
@@ -122,30 +128,38 @@ def D_T(I):
     # ADS
     # eqns 26 and 27
     # WHAT ARE OUR TIME STEPS, MONTHS???
-    delta = 1e-2
+    delta = 0.5
     M = I_to_M(I)
+    print(M)
     D = 0
     for s in np.argwhere(I != 0):
         D += np.log(T*p_n(*Ms_minus(s, M)))
-    D -= sum(p(int(s), M[:, int(s)])*delta for s in np.arange(0, T + delta, delta))
+    D -= sum(p_n(int(s), M[:, int(s)])*delta for s in np.arange(0, T, delta))
     return D
 
 def Z_T(Sn, I):
+    print('Computing Z_T')
     # ADS
     Z = 1
     CT = I_to_CT(I)
-    # for i in range(1, n+1):
-    #     s = np.argwhere(M[i] == 1)[0]
-    #     x = np.log(p_i_n(*Ms_minus(s, M))/q_i_n(*Ms_minus(s, M)))
-    #     x -=
-    # WHAT IS THE MIN STATEMENT SUPPOSSED TO BE???
-    return np.exp(min(S[-1], T) * theta - CT * np.log(T * theta) + D_T(I))
+    D = D_T(I)
+    print(D)
+    #print(CT)
+    #print(theta)
+    #print(min(S[-1], T))
+    return np.exp(min(S[-1], T) * theta - CT * np.log(T * theta) + D)
 
 # JCS
-def run_IS_algorithm():
-    # generate event times using poisson
-    # for each Sm draw Im
-    # check if its a rare event, if so, generate Z_T and define as Yn
-    S = sample_S()
-    I = sample_I(S)
-    return np.mean(Z_T(S,I))
+# generate event times using poisson
+# for each Sm draw Im
+# check if its a rare event, if so, generate Z_T and define as Yn
+S = sample_S()
+I = sample_I(S)
+
+n_samples = 1
+samples = []
+for _ in range(n_samples):
+    samples += [Z_T(S,I)]
+print(samples)
+    
+
