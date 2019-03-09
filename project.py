dff209c20189e6d1d7bee5ff7997c0d410f0ba24
Colin Gaffney <cgaffney@stanford.edu>
2019-03-08 17:21:08 -0800

    a

--- project.py
+++ project.py
@@ -1,20 +1,22 @@
 import numpy as np
 import math
 
-T = 5 * 12
+discretization = 12
+T = 1
+timesteps = T * discretization
 mu = 0.2
 n = 100
 
-theta = 1. / T * math.ceil(mu * n)
+theta = (1. / timesteps) * math.ceil(mu * n)
 
-# Parameters for calculation of p_i_n: Note that c is defined as theta in the paper cited by KG.
-kappa = np.random.uniform(0.5, 1.5, n)
-c = np.random.uniform(0.001, 0.051, n)
+# Parameters for calculation of p_i_n: Note that c is defined as theta in the paper cited by KG >:(
+kappa = np.random.uniform(0.5, 1.5, n) / 4.
+c = np.random.uniform(0.001, 0.051, n) / 4. # Note: this adjusts the units of time for c from quarters to months
 sigma_tilde = np.random.uniform(0, 0.2, n)
 X_0 = np.array(c) # Make sure to review if correct.
 
 # Construct sigma array:
-sigma = [min(np.sqrt(2 * kappa[i] * c[i]), sigma_tilde[i]) for i in range(n)]
+sigma = [min(np.sqrt(2 * kappa[i] * c[i]), sigma_tilde[i]) / np.sqrt(4.) for i in range(n)]
 
 # Construct gamma array:
 gamma = [np.sqrt(kappa[i]**2 + (2 * sigma[i]**2)) for i in range(n)]
@@ -39,10 +41,10 @@ def sample_I(event_times):
     # call q_i_n and q_n
     # returns T-vector with values between 1 and n. Element is the id of the firm that defaults at that timestep. 0 if no one defaults
 
-    M = np.zeros((n, T))
+    M = np.zeros((n, timesteps))
     print('Sampling I')
     for i, Sm in enumerate(event_times):
-        if Sm > T:
+        if Sm > timesteps:
             break
         print((i, Sm))
         if i == 0:
@@ -90,8 +92,9 @@ def p_i_n(t, Mt):
 
 # Sum of individual p_i_n (at a given time-step):
 def p_n(t, state_B):
-    probs = p_i_n(t, state_B)
-    return np.sum(probs)
+    rates = p_i_n(t, state_B)
+    #print(np.sum(rates))
+    return np.sum(rates)
 
 # JCS
 
@@ -115,8 +118,8 @@ def I_to_CT(I):
     return CT
 
 def I_to_M(I):
-    M = np.zeros((n, T))
-    for i in range(T):
+    M = np.zeros((n, timesteps))
+    for i in range(timesteps):
         if I[i] > 0:
             M[int(I[i]) - 1, i:] = 1
     return M
@@ -133,9 +136,13 @@ def D_T(I):
     delta = 0.5
     M = I_to_M(I)
     D = 0
-    for s in np.argwhere(I != 0):
-        D += np.log(T*p_n(*Ms_minus(s, M)))
-    D -= sum(p_n(int(s), M[:, int(s)])*delta for s in np.arange(0, T, delta))
+    print(np.squeeze(np.argwhere(I != 0)))
+    for s in np.squeeze(np.argwhere(I != 0)):
+        D += np.log(timesteps*p_n(*Ms_minus(s, M)))
+    print(D)
+    print([p_n(int(s), M[:, int(s)])*delta for s in np.arange(0, timesteps, delta)])
+    print(sum(p_n(int(s), M[:, int(s)])*delta for s in np.arange(0, timesteps, delta)))
+    D -= sum(p_n(int(s), M[:, int(s)])*delta for s in np.arange(0, timesteps, delta))
     return D
 
 def Z_T(Sn, I):
@@ -144,21 +151,24 @@ def Z_T(Sn, I):
     Z = 1
     CT = I_to_CT(I)
     D = D_T(I)
+    print(CT)
+    print(theta)
+    print(min(Sn[-1], timesteps))
+    print(min(Sn[-1], timesteps) * theta)
+    print(- CT * np.log(timesteps * theta))
     print(D)
-    #print(CT)
-    #print(theta)
-    #print(min(S[-1], T))
-    return np.exp(min(Sn[-1], T) * theta - CT * np.log(T * theta) + D)
+    return np.exp(min(Sn[-1], timesteps) * theta - CT * np.log(timesteps * theta) + D)
 
 # JCS
 # generate event times using poisson
 # for each Sm draw Im
 # check if its a rare event, if so, generate Z_T and define as Yn
-S = sample_S()
-I = sample_I(S)
+
 
 n_samples = 1
 samples = []
 for _ in range(n_samples):
+    S = sample_S()
+    I = sample_I(S)
     samples += [Z_T(S,I)]
 print(samples)
